84c5e1d60c4eed12dfe45decdcd34e8c5356d3dc
savan513 <savanbudheliya513@gmail.com>
2023-12-04 21:38:48 +0530

    Updated CORS DNS value

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const allowedOrigins = ['http://localhost:3000','https://lovely-cannoli-fd0620.netlify.app/'];
+const allowedOrigins = ['https://lovely-cannoli-fd0620.netlify.app','http://localhost:3000'];
 const corsOptions = {
   origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
