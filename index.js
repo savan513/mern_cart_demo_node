a0071a6642fdedcab1e0ca463d6ce8a8b4607a5f
savan513 <savanbudheliya513@gmail.com>
2023-12-04 21:17:41 +0530

    Added CORS DNS value

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 
-const allowedOrigins = ['http://localhost:3001'];
+const allowedOrigins = ['https://merncartdemonode.onrender.com/'];
 const corsOptions = {
   origin: function (origin, callback) {
     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
